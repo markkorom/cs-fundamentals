3984b482a66f970d55e74326f35a0bad13e5ee8a
Mark Korom <mark@splendex.io>
2020-09-13 10:04:22 +0200

    first commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Example GradeBook");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(30.5);
+            book.AddGrade(10.5);
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The average grade is {stats.Average}");
+        }
+    }
+}
---
+++ test/GradeBook.Tests/BookTests.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void BookCalculatesAnAverageGrade()
+        {
+            // arrange
+            var book = new Book("Test");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
+
+            // act
+            var stats = book.GetStatistics();
+
+            // assert
+            Assert.Equal(85.6, stats.Average, 1);
+            Assert.Equal(90.5, stats.High, 1);
+            Assert.Equal(77.3, stats.Low, 1);
+
+        }
+    }
+}
