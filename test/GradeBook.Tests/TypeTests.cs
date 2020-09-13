73e88cf6eca92ee17d21df5bc18d38cb99b45226
Mark Korom <mark@splendex.io>
2020-09-13 11:00:43 +0200

    feat: finish working with reference types and value types chapter

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -28,10 +28,81 @@ namespace GradeBook.Tests
             Assert.Equal("Book 2", book2.Name);
             Assert.NotSame(book1, book2);
         }
-
         Book GetBook(string name)
         {
             return new Book(name);
+        }
+
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+            var book1 = GetBook("Book 1");
+            SetName(book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+
+        private void SetName(Book book, string name)
+        {
+            book.Name = name;
+        }
+        [Fact]
+        public void CSharpIsPassByValue()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+
+            Assert.Equal("Book 1", book1.Name);
+        }
+        private void GetBookSetName(Book book, string name)
+        {
+            book = new Book(name);
+        }
+        [Fact]
+        public void CSharpCanPassByRef()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(out book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+        private void GetBookSetName(out Book book, string name)
+        {
+            book = new Book(name);
+        }
+        [Fact]
+        public void ValueTypeAlsoPassByValue()
+        {
+            var x = GetInt();
+            SetInt(ref x);
+
+            Assert.Equal(42, x);
+        }
+
+        private void SetInt(ref int x)
+        {
+            x = 42;
+        }
+
+        private int GetInt()
+        {
+            return 3;
+        }
+
+        [Fact]
+        public void StringsBehaveLikeValueTypes()
+        {
+            string name = "Mark";
+            var upper = MakeUppercase(name);
+
+            Assert.Equal("Mark", name);
+            Assert.Equal("MARK", upper);
+        }
+
+        private string MakeUppercase(string param)
+        {
+            return param.ToUpper();
         }
     }
 }
